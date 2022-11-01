6cc2bf23c029fc288c324393ada84d502863b69e
hyeonginju <guddls64@naver.com>
2022-11-01 16:26:11 +0900

    prettier setting

--- .prettierrc.js
+++ .prettierrc.js
@@ -4,5 +4,5 @@ module.exports = {
   semi: true, // 세미콜론 사용여부
   trailingComma: 'all', // 여러 줄을 사용할 때, 후행 콤마 사용 방식
   arrowParens: 'always', // 화살표 함수 괄호 사용 방식 () => {}
-  tabWidth: 2, // 탭 너비
+  tabWidth: 2, //  탭 너비
 };
